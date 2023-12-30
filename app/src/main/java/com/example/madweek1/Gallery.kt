ae9f20dda54da2f182e9847cc933552c2ce651fe
name <email>
2023-12-30 11:42:54 +0900

    image id finished

--- app/src/main/java/com/example/madweek1/Gallery.kt
+++ app/src/main/java/com/example/madweek1/Gallery.kt
@@ -15,6 +15,7 @@ import android.util.Log
 import android.widget.TextView
 import kotlinx.parcelize.Parcelize
 
+
 @Parcelize
 data class ImageItem(
     val id: Int,
@@ -23,54 +24,63 @@ data class ImageItem(
     val date: String,
     val camera: String
 ) : Parcelable
+
+@Parcelize
+data class ImageResource(
+    val id: Int,
+    val address: Int
+) : Parcelable
+
+
+
 interface OnImageClickListener {
-    fun onImageClick(imageId: Int)
+    fun onImageClick(imageId: Int, imageAddress: Int)
 }
 class Gallery : Fragment(), OnImageClickListener {
-    val imageIds = listOf(
-        R.drawable.a,
-        R.drawable.b,
-        R.drawable.c,
-        R.drawable.d,
-        R.drawable.e,
-        R.drawable.f,
-        R.drawable.g,
-        R.drawable.h,
-        R.drawable.i,
-        R.drawable.j,
-        R.drawable.k,
-        R.drawable.l,
-        R.drawable.m,
-        R.drawable.n,
-        R.drawable.o,
-        R.drawable.p,
-        R.drawable.q,
-        R.drawable.r,
-        R.drawable.s,
-        R.drawable.t
+    val imageIds = arrayListOf<ImageResource>(
+        ImageResource(0, R.drawable.a),
+        ImageResource(1, R.drawable.b),
+        ImageResource(2, R.drawable.c),
+        ImageResource(3, R.drawable.d),
+        ImageResource(4, R.drawable.e),
+        ImageResource(5, R.drawable.f),
+        ImageResource(6, R.drawable.g),
+        ImageResource(7, R.drawable.h),
+        ImageResource(8, R.drawable.i),
+        ImageResource(9, R.drawable.j),
+        ImageResource(10, R.drawable.k),
+        ImageResource(11, R.drawable.l),
+        ImageResource(12, R.drawable.m),
+        ImageResource(13, R.drawable.n),
+        ImageResource(14, R.drawable.o),
+        ImageResource(15, R.drawable.p),
+        ImageResource(16, R.drawable.q),
+        ImageResource(17, R.drawable.r),
+        ImageResource(18, R.drawable.s),
+        ImageResource(19, R.drawable.t),
     )
-    var ImageList = arrayListOf<ImageItem>(
-        ImageItem(2131165395,"emerald ocean", "Seogwipo", "2023-12-23", "galaxy 10"),
-        ImageItem(2131165396, "stone island", "Jeju", "2023-12-24", "apple 20"),
-        ImageItem(2131165397, "lights", "Daejeon", "2023-12-25", "olympus 55"),
-        ImageItem(2131165398, "windmill", "Daejeon", "2023-12-26", "apple 20"),
-        ImageItem(2131165399, "sky", "Jeju", "2023-12-25", "olympus 55"),
-        ImageItem(2131165400, "lights", "Daejeon", "2023-12-25", "apple 20"),
-        ImageItem(2131165401, "lights", "Seogwipo", "2023-12-23", "olympus 55"),
-        ImageItem(2131165402, "lights", "Seogwipo", "2023-12-25", "apple 20"),
-        ImageItem(2131165403, "lights", "Jeju", "2023-12-23", "olympus 55"),
-        ImageItem(2131165404, "lights", "Daejeon", "2023-12-25", "galaxy 10"),
-        ImageItem(2131165405, "seaOfMarseille", "Marseille", "2022-09-24", "iphone 12 pro"),
-        ImageItem(2131165406, "sailing", "France", "2022-11-14", "canon"),
-        ImageItem(2131165407, "Eiffel Tower", "Paris", "2023-01-01", "canon"),
-        ImageItem(2131165408, "Chair and Man", "Paris", "2022-08-24", "iphone 12"),
-        ImageItem(2131165409, "Library", "UC Berkely", "2023-08-30", "iphone 12"),
-        ImageItem(2131165410, "pont", "Giverny", "2022-09-23", "canon"),
-        ImageItem(2131165411, "Lyon", "Lyon", "2022-09-21", "iphone 12"),
-        ImageItem(2131165412, "Rock n Sea", "Nice", "2022-09-03", "canon"),
-        ImageItem(2131165413, "Metro", "Praha", "2022-12-23", "iphone 12"),
-        ImageItem(2131165414, "Harbor", "Marseille", "2022-10-25", "canon"),
 
+    var ImageList = arrayListOf<ImageItem>(
+        ImageItem(0,"emerald ocean", "Seogwipo", "2023-12-23", "galaxy 10"),
+        ImageItem(1, "stone island", "Jeju", "2023-12-24", "apple 20"),
+        ImageItem(2, "lights", "Daejeon", "2023-12-25", "olympus 55"),
+        ImageItem(3, "windmill", "Daejeon", "2023-12-26", "apple 20"),
+        ImageItem(4, "sky", "Jeju", "2023-12-25", "olympus 55"),
+        ImageItem(5, "lights", "Daejeon", "2023-12-25", "apple 20"),
+        ImageItem(6, "lights", "Seogwipo", "2023-12-23", "olympus 55"),
+        ImageItem(7, "lights", "Seogwipo", "2023-12-25", "apple 20"),
+        ImageItem(8, "lights", "Jeju", "2023-12-23", "olympus 55"),
+        ImageItem(9, "lights", "Daejeon", "2023-12-25", "galaxy 10"),
+        ImageItem(10, "seaOfMarseille", "Marseille", "2022-09-24", "iphone 12 pro"),
+        ImageItem(11, "sailing", "France", "2022-11-14", "canon"),
+        ImageItem(12, "Eiffel Tower", "Paris", "2023-01-01", "canon"),
+        ImageItem(13, "Chair and Man", "Paris", "2022-08-24", "iphone 12"),
+        ImageItem(14, "Library", "UC Berkely", "2023-08-30", "iphone 12"),
+        ImageItem(15, "pont", "Giverny", "2022-09-23", "canon"),
+        ImageItem(16, "Lyon", "Lyon", "2022-09-21", "iphone 12"),
+        ImageItem(17, "Rock n Sea", "Nice", "2022-09-03", "canon"),
+        ImageItem(18, "Metro", "Praha", "2022-12-23", "iphone 12"),
+        ImageItem(19, "Harbor", "Marseille", "2022-10-25", "canon"),
         )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -87,10 +97,11 @@ class Gallery : Fragment(), OnImageClickListener {
 
         return view
     }
-    override fun onImageClick(imageId: Int) {
+    override fun onImageClick(imageId: Int, imageAddress: Int) {
 
         val bundle = Bundle()
         bundle.putInt("image_id", imageId)
+        bundle.putInt("image_address", imageAddress)
         bundle.putParcelableArrayList("image_list", ImageList)
 
         val detailFragment = DetailFragment()
@@ -106,7 +117,7 @@ class Gallery : Fragment(), OnImageClickListener {
 }
 
 
-class ImageAdapter(private val context: Context, private val imageIds: List<Int>, private val ImageList: List<ImageItem>, private val listener: OnImageClickListener) :
+class ImageAdapter(private val context: Context, private val imageIds: List<ImageResource>, private val ImageList: List<ImageItem>, private val listener: OnImageClickListener) :
     RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
 
     class ViewHolder(val imageView: ImageView) : RecyclerView.ViewHolder(imageView)
@@ -124,9 +135,9 @@ class ImageAdapter(private val context: Context, private val imageIds: List<Int>
         return ViewHolder(imageView)
     }
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.imageView.setImageResource(imageIds[position])
+        holder.imageView.setImageResource(imageIds.map { it.address }[position])
         holder.imageView.setOnClickListener {
-            listener.onImageClick(imageIds[position])
+            listener.onImageClick(imageIds.map { it.id }[position], imageIds.map { it.address }[position])
         }
     }
 
@@ -144,7 +155,8 @@ class DetailFragment : Fragment() {
         val cameraTextView: TextView = view.findViewById(R.id.camera)
 
         val imageId = arguments?.getInt("image_id", 0) ?: 0
-        imageView.setImageResource(imageId)
+        val imageAdress = arguments?.getInt("image_address", 0) ?: 0
+        imageView.setImageResource(imageAdress)
         println(imageId)
 
         val imageItems: ArrayList<ImageItem>? = arguments?.getParcelableArrayList("image_list")
