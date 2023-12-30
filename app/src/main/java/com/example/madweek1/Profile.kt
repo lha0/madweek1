4f27e6823dd9277d827fe8750be5770008b7ca01
이하영 <hayeong@ihayeong-ui-MacBookPro.local>
2023-12-30 16:52:23 +0900

    전화걸기 기능 추가

--- app/src/main/java/com/example/madweek1/Profile.kt
+++ app/src/main/java/com/example/madweek1/Profile.kt
@@ -2,6 +2,9 @@ package com.example.madweek1
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,6 +14,8 @@ import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import org.w3c.dom.Text
 
@@ -21,24 +26,32 @@ class Profile : Fragment() {
     ): View?
     {
         val view = inflater.inflate(R.layout.profile, container, false)
+        val CALL_REQUEST_CODE = 1
 
         val name = view.findViewById<TextView>(R.id.profileName)
         val number = view.findViewById<TextView>(R.id.profileNumber)
-        val button = view.findViewById<Button>(R.id.button)
+        val listbutton = view.findViewById<Button>(R.id.listbutton)
+        val callbutton = view.findViewById<Button>(R.id.callbutton)
 
         val phoneNumberItem = arguments?.getSerializable("phoneNumberItem") as PhoneNumberItem
         name.text = phoneNumberItem.name
         number.text = phoneNumberItem.number
 
-        button.setOnClickListener {
+        listbutton.setOnClickListener {
             val fragmentManager = (context as AppCompatActivity).supportFragmentManager
             val beforeFragment = PhoneBook()
 
-            // 프로필 프래그먼트를 팝업창으로 보여줍니다.
+            // 목록으로 돌아가기
             fragmentManager.beginTransaction()
                 .replace(R.id.popup, beforeFragment)
                 .addToBackStack(null)
                 .commit()
+        }
+
+        callbutton.setOnClickListener{
+                var intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel: "+ phoneNumberItem.number)
+                startActivity(intent)
         }
 
 
